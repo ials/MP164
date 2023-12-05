76b8fb1fb61091d4e305233c9cc777164f833571
Ivan Lizarazo <ials@Ivans-MacBook-Air.local>
2023-12-05 17:08:22 -0500

    6th try

--- conf.py
+++ conf.py
@@ -23,6 +23,7 @@ author = 'Ivan Lizarazo'
 
 
 # -- General configuration ---------------------------------------------------
+import sphinx_rtd_theme
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
